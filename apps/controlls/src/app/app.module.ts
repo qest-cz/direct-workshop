21338e132a2350ab0c564a6b1beff2c1225a276e
Dan Jarkovský <dan.jarkovsky@qest.cz>
2023-06-14 07:48:58 +0200

    init

--- apps/controlls/src/app/app.module.ts
+++ apps/controlls/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { RedisClientModule } from '@pawesome-care/redis-client';
+import { RedisClientModule } from '@pawesome-care/redis-client';
 
 @Module({
   imports: [
     // HINT: Uncomment the line below to inject the RedisClientModule
-    // RedisClientModule,
+    RedisClientModule,
   ],
   controllers: [AppController],
   providers: [AppService],
